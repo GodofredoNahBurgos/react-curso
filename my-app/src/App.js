09c5c8f7dcd6e472715545a81b8163ca19cd51a6
GodofredoNahBurgos <skate_forever_1990@hotmail.com>
2022-10-15 18:20:27 -0500

    Primer Commit del Curso

---
+++ my-app/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Componente from './components/Componente';
+import './App.css';
+import Propiedades from './components/Propiedades';
+
+class App extends Component {
+
+  render() {
+
+    let nombre = "Jhon";
+    let auth = false;
+    let estaciones = ["Primavera", "Verano", "Otoño", "Inverno"];
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+          <label htmlFor='nombre'>Nombre</label>
+          <input type="text" id="nombre"></input>
+          <h1>{nombre}</h1>
+          <p>{auth ? "El usuario esta logeado":"El usuario no esta logeado"}</p>
+          <ul>
+            {estaciones.map((elemento, index) => (<li key={index}>{elemento}</li>))}
+          </ul>
+        <section>
+          <Componente msg="Hola, soy un componente funcional expresado"/>
+          <Propiedades 
+          cadena="Esto es una cadena de texto" 
+          numero={19} 
+          booleano={false}
+          arreglo={[1,2,3]}
+          objeto={{nombre:"jhon", correo: "ejemplo@ejemplo.com" }}
+          elementoReact={<li>Esto es un elemento React</li>}
+          funcion={(num) => num*num}
+          componenteReact={
+            <Componente msg="Soy un componente pasado en Props"/>
+          }/>
+        </section>
+
+
+        </div>
+        <p className="App-intro">
+          To get started, editar <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/components/Componente.js
@@ -0,0 +1,14 @@
+//import React,{ Component } from "react";
+import React from "react";
+
+const Componente = props => <h2>{props.msg}</h2>
+
+// Basado en clases
+// class Componente extends Component{
+//     render(){
+//         return <h2>{this.props.msg}</h2>
+//     }
+
+// }
+
+export default Componente;
