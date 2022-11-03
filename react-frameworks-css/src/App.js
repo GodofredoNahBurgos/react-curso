003713c05f2207dffc3b1bb9bfed32e2af61500c
GodofredoNahBurgos <skate_forever_1990@hotmail.com>
2022-11-03 15:12:27 -0600

    CSS Frameworks con React

--- react-frameworks-css/src/App.js
+++ react-frameworks-css/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import Bulma from './components/Bulma';
+import MaterialUi from './components/MaterialUI';
+//import Bootstrap from './components/Bootstrap';
+//import Bulma from './components/Bulma';
+//import ReactBootstrap from './components/ReactBootstrap';
 
 function App() {
   return (
     <div>
       <h1>Frameworks CSS con React</h1>
       {/*<Bootstrap/>*/}
-      <Bulma/>
+      {/*<Bulma/>*/}
+      {/*<ReactBootstrap />*/}
+      <MaterialUi/>
     </div>
   );
 }
---
+++ react-frameworks-css/src/components/MaterialUI.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MediaCard from './MaterialCard';
+import MaterialDrawer from './MaterialDrawer';
+
+export default function MaterialUi(){
+    return(
+        <>
+        <h2>Material Ui</h2>
+        <MediaCard/>
+        <MaterialDrawer/>
+        </>
+    );
+}
